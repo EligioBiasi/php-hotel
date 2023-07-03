76c1b01e6cce285478a7709df7ded8dba343a4ca
EligioBiasi <biasi.eligio@edufaimm.it>
2023-07-03 15:26:38 +0200

    set up for table

--- index.php
+++ index.php
@@ -1,5 +1,45 @@
 <?php
 
+    $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+
+    ];
+
 ?>
 
 <!DOCTYPE html>
@@ -8,8 +48,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>php-hotel</title>
+    <!-- Bootstrap 5.2 -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <!-- my style -->
+    <link rel="stylesheet" href="./style.css">
 </head>
     <body>
-        
+        <main>
+            <ul>
+                <?php foreach($hotels as $singleHotel){?>
+                <li>
+                    <?php 
+                    $hotelName = $singleHotel['name'];
+                    $hotelDescription = $singleHotel['description'];
+                    $hotelParking = $singleHotel['parking'];
+                    $hotelVote = $singleHotel['vote'];
+                    $hotel_distance_to_center = $singleHotel['distance_to_center'];
+
+                    
+                    echo $hotelName . '-' . $hotelDescription . '-' . $hotelParking . '-' . $hotelVote . '-' . $hotel_distance_to_center?>
+                </li>
+                <?php }?>
+
+            </ul>
+        </main>
     </body>
 </html>
